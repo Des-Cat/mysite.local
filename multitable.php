730641d134f4ddc7e6933de430e9f209dc3efe7c
Dobruy_Kot <Paradise13>
2020-10-15 14:51:19 +0300

    Multi table

---
+++ multitable.php
@@ -0,0 +1,29 @@
+<?php require_once "include/multi.php" ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Nix Education Beginner</title>
+    <link rel="stylesheet" href=public/css/style.css>
+</head>
+<body>
+<div id="table">
+    <table>
+        <tr>
+           <td><?php multi(1)?></td>
+           <td><?php multi(2)?></td>
+           <td><?php multi(3)?></td>
+           <td><?php multi(4)?></td>
+           <td><?php multi(5)?></td>
+        </tr>
+        <tr>
+           <td><?php multi(6)?></td>
+           <td><?php multi(7)?></td>
+           <td><?php multi(8)?></td>
+           <td><?php multi(9)?></td>
+           <td><?php multi(10)?></td>
+        </tr>
+    </table>
+</div>
+</body>
+</html>
